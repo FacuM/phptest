5c1e580483fc45d3db78bbdf4a9e40f5457cc1e5
Facundo Montero <facumo.fm@gmail.com>
2021-02-17 02:44:07 -0300

    Add missing /uno and /dos pages.

---
+++ dos.php
@@ -0,0 +1,13 @@
+<?php
+
+    require_once 'view/header.php';
+
+    require_once 'include/main.php';
+
+?>
+
+<h1> /dos </h1>
+
+<p> Esta es la segunda página pública. </p>
+
+<?php require_once 'view/footer.php'; ?>
---
+++ uno.php
@@ -0,0 +1,13 @@
+<?php
+
+    require_once 'view/header.php';
+
+    require_once 'include/main.php';
+
+?>
+
+<h1> /uno </h1>
+
+<p> Esta es la primera página pública. </p>
+
+<?php require_once 'view/footer.php'; ?>
